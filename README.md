d5b037602565732de98960b33b0b5c8d66a1d7b0
ericsooooooog <eric.song.life@gmail.com>
2025-10-06 13:03:44 -0700

    misc: initial commit, upload week1 folder

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SWE262P
+
+## week1
+
+```shell
+cd ./week1
+g++ Main.cpp -o Main -std=c++11
+./Main ../pride-and-prejudice.txt
+```
---
+++ week1/Main.cpp
@@ -0,0 +1,110 @@
+/* ************************************************************************
+> File Name:     Main.cpp
+> Author:        Zhenyu Song
+> Mail:          zhenyus4@uci.edu
+> Created Time:  Mon Oct  6 00:19:10 2025
+> Description:   
+ ************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cctype>
+#include <sstream>
+
+const std::string STOP_WORDS_PATH = "../stop_words.txt";
+const int MAX_COUNT = 25;
+
+bool loadStopWords(const std::string& filename, std::unordered_set<std::string>& stopWords) {
+    std::ifstream file(filename);
+    std::string line;
+    
+    if (!file.is_open()) {
+        std::cerr << "failed to open " << filename << std::endl;
+        return false;
+    }
+    
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string word;
+        while (std::getline(ss, word, ',')) {
+            stopWords.insert(word);
+        }
+    }
+
+    file.close();
+    
+    return true;
+}
+
+void toLower(std::string& str) {
+    std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) { 
+        return std::tolower(c); 
+    });
+}
+
+void processLine(const std::string& line, const std::unordered_set<std::string>& stopWords, std::unordered_map<std::string, int>& wordCounts) {
+    if (line.empty()) return;
+    
+    auto dummyLine = line + " ";
+    std::string word;
+    for (char ch : dummyLine) {
+        if (isalnum(ch)) {
+            word += ch;
+            continue;
+        }
+
+        toLower(word);
+        
+        if (word.length() < 2 || stopWords.count(word)) {
+            word.clear();
+            continue;
+        }
+        ++wordCounts[word];
+        word.clear();
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "wrong usage, please enter: " << argv[0] << " <filename>" << std::endl;
+        return 1;
+    }
+    
+    std::unordered_set<std::string> stopWords;
+    if (!loadStopWords(STOP_WORDS_PATH, stopWords)) {
+        std::cerr << "failed to load stop words" << std::endl;
+        return 1;
+    }
+    
+    std::unordered_map<std::string, int> wordCounts;
+    
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open()) {
+        std::cerr << "failed to open " << argv[1] << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        processLine(line, stopWords, wordCounts);
+    }
+    inputFile.close();
+    
+    std::vector<std::pair<std::string, int>> wordVec(wordCounts.begin(), wordCounts.end());
+    
+    std::sort(wordVec.begin(), wordVec.end(), [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+        return a.second > b.second;
+    });
+    
+    const int numToPrint = std::min(MAX_COUNT, static_cast<int>(wordVec.size()));
+    for (int i = 0; i < numToPrint; ++i) {
+        std::cout << wordVec[i].first << " - " << wordVec[i].second << std::endl;
+    }
+    
+    return 0;
+}
